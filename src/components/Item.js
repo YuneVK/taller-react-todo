846a2e13bb007044ea191d4beca7fa43484efc0a
Sonia Ruiz <yune.vk@gmail.com>
2019-09-10 17:01:11 +0200

    Add project

---
+++ src/components/Item.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Item = props => {
+  return (
+    <li
+      className={`Item${props.isComplete ? " completed" : ""}`}
+      onClick={() => props.completeItem(props.index)}
+    >
+      {props.content}
+    </li>
+  );
+};
+
+export default Item;
